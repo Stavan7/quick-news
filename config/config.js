d9614467fc0a453f6055dbaa476c6f13abc5476c
Stavan7 <kulkarnistavan25@gmail.com>
2023-05-03 21:04:46 +0530

    updates

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
+import { NEWS_API_KEY } from '@env'
+
 export const country = 'in';
 export const language = "en";
-export const API_KEY = " "; //add you API key here
+export const API_KEY = NEWS_API_KEY; //add you API key here
 export const api_endpoint = "https://newsapi.org/v2/top-headlines";
 export const everything_endpoint = "https://newsapi.org/v2/everything";
