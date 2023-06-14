4e16b638bd8584df3d178df75d6f03e328257ebf
Stavan7 <kulkarnistavan25@gmail.com>
2023-06-14 21:44:08 +0530

    share areticle bug fixed

--- components/NewsCard.js
+++ components/NewsCard.js
@@ -1,141 +1,141 @@
 import React from 'react';
-import {
-    Text,
-    View,
-    Share,
-    StyleSheet,
-    TouchableOpacity
-} from 'react-native';
+import {Text, View, Share, StyleSheet, TouchableOpacity} from 'react-native';
 import moment from 'moment';
 import FastImage from 'react-native-fast-image';
 import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
-const NewsCard = ({ data, navigation }) => {
-
-    const onShare = async () => {
-        try {
-            const result = await Share.share({
-                message: `Read this article: ${url}`,
-            });
-            if (result.action === Share.sharedAction) {
-                if (result.activityType) {
-                    console.log(result.action)
-                } else {
-                    console.log(result.action)
-                }
-            } else if (result.action === Share.dismissedAction) {
-                console.log(result.action)
-            }
-        } catch (error) {
-            alert(error.message);
-        }
-    }
-
-    const ImgCheck = () => {
-        if (data.urlToImage !== null) {
-            return (
-                <FastImage
-                    source={{ uri: data.urlToImage }}
-                    style={styles.image}
-                    alt="Alternate Text"
-                    resizeMode={"cover"}
-                />
-            )
-        } else if (data.source.name === 'Google News') {
-            return (
-                <FastImage
-                    source={require('../assets/discover/google-news.png')}
-                    style={styles.image}
-                    alt="Alternate Text"
-                    resizeMode={"cover"}
-                />
-            )
+const NewsCard = ({data, navigation}) => {
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: `Read this article: ${data.url}`,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log(result.action);
         } else {
-            return (
-                <FastImage
-                    source={require('../assets/discover/noImage.jpg')}
-                    style={styles.image}
-                    alt="Alternate Text"
-                    resizeMode={"cover"}
-                />
-            )
+          console.log(result.action);
         }
+      } else if (result.action === Share.dismissedAction) {
+        console.log(result.action);
+      }
+    } catch (error) {
+      alert(error.message);
     }
+  };
 
-    return (
-        <TouchableOpacity activeOpacity={0.8}
-            onPress={() => navigation.navigate('NoBottomTab', {
-                screen: "DetailsScreen",
-                params: { data }
-            })}>
+  const ImgCheck = () => {
+    if (data.urlToImage !== null) {
+      return (
+        <FastImage
+          source={{uri: data.urlToImage}}
+          style={styles.image}
+          alt="Alternate Text"
+          resizeMode={'cover'}
+        />
+      );
+    } else if (data.source.name === 'Google News') {
+      return (
+        <FastImage
+          source={require('../assets/discover/google-news.png')}
+          style={styles.image}
+          alt="Alternate Text"
+          resizeMode={'cover'}
+        />
+      );
+    } else {
+      return (
+        <FastImage
+          source={require('../assets/discover/noImage.jpg')}
+          style={styles.image}
+          alt="Alternate Text"
+          resizeMode={'cover'}
+        />
+      );
+    }
+  };
 
-            <View style={styles.dataContainer}>
-                <ImgCheck />
-                <Text style={styles.title} numberOfLines={1}>{data.title}</Text>
-                <View style={styles.bottomContainer}>
-                    <View flexDirection='row'>
-                        <Text style={styles.source}>Source : {data.source.name ?? 'Not Available'}</Text>
-                        <Text style={styles.time}>{moment(data.publishedAt).format('LT')}</Text>
-                    </View>
-                    <EvilIcons
-                        name="share-google"
-                        size={28}
-                        color={'white'}
-                        onPress={onShare}
-                        style={{ marginRight: 15 }}
-                    />
-                </View>
-            </View>
-        </TouchableOpacity>
-    )
-
-}
+  return (
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() =>
+        navigation.navigate('NoBottomTab', {
+          screen: 'DetailsScreen',
+          params: {data},
+        })
+      }>
+      <View style={styles.dataContainer}>
+        <ImgCheck />
+        <Text style={styles.title} numberOfLines={1}>
+          {data.title}
+        </Text>
+        <View style={styles.bottomContainer}>
+          <View flexDirection="row">
+            <Text style={styles.source}>
+              Source : {data.source.name ?? 'Not Available'}
+            </Text>
+            <Text style={styles.time}>
+              {moment(data.publishedAt).format('LT')}
+            </Text>
+          </View>
+          <EvilIcons
+            name="share-google"
+            size={28}
+            color={'white'}
+            onPress={onShare}
+            style={{marginRight: 15}}
+          />
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    dataContainer: {
-        borderRadius: 10,
-        marginBottom: 20,
-        marginHorizontal: 20,
-        height: 320,
-        backgroundColor: '#191919',
-        shadowColor: '#121212',
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.34,
-        shadowRadius: 6.27,
-        elevation: 10,
+  dataContainer: {
+    borderRadius: 10,
+    marginBottom: 20,
+    marginHorizontal: 20,
+    height: 320,
+    backgroundColor: '#191919',
+    shadowColor: '#121212',
+    shadowOffset: {
+      width: 0,
+      height: 5,
     },
-    image: {
-        height: '73%',
-        width: '100%',
-        borderTopRightRadius: 10,
-        borderTopLeftRadius: 10,
-    },
-    title: {
-        fontSize: 14,
-        color: 'white',
-        margin: 10,
-        fontFamily: 'Poppins-SemiBold'
-    },
-    bottomContainer: {
-        marginTop: 5,
-        flexDirection: 'row',
-        marginHorizontal: 10,
-        justifyContent: 'space-between',
-    },
-    source: {
-        fontSize: 13,
-        color: '#fff',
-        fontFamily: 'Poppins-Regular'
-    },
-    time: {
-        fontSize: 13,
-        color: '#fff',
-        marginLeft: 10,
-        fontFamily: 'Poppins-Regular'
-    }
-})
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+    elevation: 10,
+  },
+  image: {
+    height: '73%',
+    width: '100%',
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+  },
+  title: {
+    fontSize: 14,
+    color: 'white',
+    margin: 10,
+    fontFamily: 'Poppins-SemiBold',
+  },
+  bottomContainer: {
+    marginTop: 5,
+    flexDirection: 'row',
+    marginHorizontal: 10,
+    justifyContent: 'space-between',
+  },
+  source: {
+    fontSize: 13,
+    color: '#fff',
+    fontFamily: 'Poppins-Regular',
+  },
+  time: {
+    fontSize: 13,
+    color: '#fff',
+    marginLeft: 10,
+    fontFamily: 'Poppins-Regular',
+  },
+});
 
-export default NewsCard; 
+export default NewsCard;
--- screens/DetailScreen.js
+++ screens/DetailScreen.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import {
-    View,
-    Text,
-    Share,
-    Linking,
-    Dimensions,
-    ScrollView,
-    StyleSheet,
-    SafeAreaView,
+  View,
+  Text,
+  Share,
+  Linking,
+  Dimensions,
+  ScrollView,
+  StyleSheet,
+  SafeAreaView,
 } from 'react-native';
 import moment from 'moment';
 import COLORS from '../constants/Colors';
@@ -17,176 +17,181 @@ import EvilIcons from 'react-native-vector-icons/EvilIcons';
 
 const HEIGHT = Dimensions.get('window').height;
 
-const DetailScreen = ({ route, navigation }) => {
+const DetailScreen = ({route, navigation}) => {
+  const data = route.params.data;
+  const image = data.urlToImage;
 
-    const data = route.params.data;
-    const image = data.urlToImage
-
-    const onShare = async () => {
-        try {
-            const result = await Share.share({
-                message: `Read this article: ${data.newsData.url}`,
-            });
-            if (result.action === Share.sharedAction) {
-                if (result.activityType) {
-                    console.log(result.action)
-                } else {
-                    console.log(result.action)
-                }
-            } else if (result.action === Share.dismissedAction) {
-                console.log(result.action)
-            }
-        } catch (error) {
-            alert(error.message);
+  const onShare = async () => {
+    try {
+      const result = await Share.share({
+        message: `Read this article: ${data.url}`,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          console.log(result.action);
+        } else {
+          console.log(result.action);
         }
+      } else if (result.action === Share.dismissedAction) {
+        console.log(result.action);
+      }
+    } catch (error) {
+      alert(error.message);
     }
+  };
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <ScrollView>
-                <View style={{ height: HEIGHT / 2.3, }}>
-                    {
-                        image !== null ?
-                            <FastImage
-                                source={{
-                                    uri: image,
-                                    priority: FastImage.priority.normal,
-                                    cache: FastImage.cacheControl.web
-                                }}
-                                style={styles.image}
-                                alt="Alternate Text"
-                                resizeMode={"cover"}
-                            />
-                            :
-                            <FastImage
-                                source={require('../assets/discover/noImage.jpg')}
-                                style={styles.image}
-                                alt="Alternate Text"
-                                resizeMode={"cover"}
-                            />
-                    }
-                    <Ionicons
-                        size={25}
-                        color="white"
-                        name="ios-chevron-back-sharp"
-                        style={styles.iconContainer}
-                        onPress={() => navigation.goBack()}
-                    />
-                    <EvilIcons
-                        size={28}
-                        color="white"
-                        name="share-apple"
-                        onPress={onShare}
-                        style={styles.shareContainer}
-                    />
-                </View>
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+        <View style={{height: HEIGHT / 2.3}}>
+          {image !== null ? (
+            <FastImage
+              source={{
+                uri: image,
+                priority: FastImage.priority.normal,
+                cache: FastImage.cacheControl.web,
+              }}
+              style={styles.image}
+              alt="Alternate Text"
+              resizeMode={'cover'}
+            />
+          ) : (
+            <FastImage
+              source={require('../assets/discover/noImage.jpg')}
+              style={styles.image}
+              alt="Alternate Text"
+              resizeMode={'cover'}
+            />
+          )}
+          <Ionicons
+            size={25}
+            color="white"
+            name="ios-chevron-back-sharp"
+            style={styles.iconContainer}
+            onPress={() => navigation.goBack()}
+          />
+          <EvilIcons
+            size={28}
+            color="white"
+            name="share-apple"
+            onPress={onShare}
+            style={styles.shareContainer}
+          />
+        </View>
 
-                <View style={styles.textContainer}>
-                    <Text style={styles.title}>{data.title}</Text>
-                    <Text style={styles.time}>Published : {moment(data.publishedAt).format('LLL')}</Text>
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{data.title}</Text>
+          <Text style={styles.time}>
+            Published : {moment(data.publishedAt).format('LLL')}
+          </Text>
 
-                    <Text style={styles.content}>{data.content ?? data.description}</Text>
-                    <Text style={styles.urlText}>To read full news, checkout :</Text>
-                    <Text style={styles.url} onPress={() => Linking.openURL(data.url)}>{data.url}</Text>
+          <Text style={styles.content}>{data.content ?? data.description}</Text>
+          <Text style={styles.urlText}>To read full news, checkout :</Text>
+          <Text style={styles.url} onPress={() => Linking.openURL(data.url)}>
+            {data.url}
+          </Text>
 
-                    <Text style={styles.authorText}>Author: {data.author ?? 'Not Available'}</Text>
-                    <Text style={styles.source}>Source: {data.source.name ?? 'Not Available'}</Text>
-                </View>
-
-            </ScrollView>
-        </SafeAreaView>
-    )
-}
+          <Text style={styles.authorText}>
+            Author: {data.author ?? 'Not Available'}
+          </Text>
+          <Text style={styles.source}>
+            Source: {data.source.name ?? 'Not Available'}
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: COLORS.screenBg
-    },
-    image: {
-        flex: 1,
-        width: '100%',
-        height: 'auto',
-    },
-    textContainer: {
-        flex: 1,
-        padding: 20,
-        marginTop: -30,
-        borderTopLeftRadius: 20,
-        borderTopRightRadius: 20,
-        backgroundColor: COLORS.screenBg,
-    },
-    title: {
-        fontSize: 16,
-        color: 'white',
-        fontFamily: 'Poppins-SemiBold'
-    },
-    time: {
-        fontSize: 14,
-        color: 'white',
-        marginTop: 10,
-        fontFamily: 'NotoSans-SemiBold'
-    },
-    content: {
-        fontSize: 16,
-        color: '#fff',
-        marginTop: 10,
-        textAlign: 'left',
-        fontFamily: 'NotoSans-Regular'
-    },
-    urlText: {
-        fontSize: 16,
-        color: 'white',
-        marginTop: 10,
-        textAlign: 'left',
-        fontFamily: 'NotoSans-Regular'
-    },
-    url: {
-        fontSize: 14,
-        color: COLORS.focusedIcon,
-        textAlign: 'left',
-        textDecorationLine: 'underline',
-        fontFamily: 'NotoSans-Regular'
-    },
-    authorText: {
-        flex: 1,
-        fontSize: 14,
-        color: 'white',
-        marginTop: 30,
-        fontFamily: 'NotoSans-Regular'
-    },
-    source: {
-        fontSize: 14,
-        color: 'white',
-        marginTop: 5,
-        fontFamily: 'NotoSans-Regular'
-    },
-    shareContainer: {
-        position: 'absolute',
-        top: 50,
-        right: 20,
-        height: 40,
-        width: 40,
-        borderRadius: 30,
-        backgroundColor: 'black',
-        textAlign: 'center',
-        textAlignVertical: 'center'
-    },
-    iconContainer: {
-        position: 'absolute',
-        top: 50,
-        left: 20,
-        height: 40,
-        width: 40,
-        borderRadius: 30,
-        textAlign: 'center',
-        textAlignVertical: 'center',
-        backgroundColor: 'black',
-    },
-    icon: {
-        height: 25,
-        width: 25,
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.screenBg,
+  },
+  image: {
+    flex: 1,
+    width: '100%',
+    height: 'auto',
+  },
+  textContainer: {
+    flex: 1,
+    padding: 20,
+    marginTop: -30,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    backgroundColor: COLORS.screenBg,
+  },
+  title: {
+    fontSize: 16,
+    color: 'white',
+    fontFamily: 'Poppins-SemiBold',
+  },
+  time: {
+    fontSize: 14,
+    color: 'white',
+    marginTop: 10,
+    fontFamily: 'NotoSans-SemiBold',
+  },
+  content: {
+    fontSize: 16,
+    color: '#fff',
+    marginTop: 10,
+    textAlign: 'left',
+    fontFamily: 'NotoSans-Regular',
+  },
+  urlText: {
+    fontSize: 16,
+    color: 'white',
+    marginTop: 10,
+    textAlign: 'left',
+    fontFamily: 'NotoSans-Regular',
+  },
+  url: {
+    fontSize: 14,
+    color: COLORS.focusedIcon,
+    textAlign: 'left',
+    textDecorationLine: 'underline',
+    fontFamily: 'NotoSans-Regular',
+  },
+  authorText: {
+    flex: 1,
+    fontSize: 14,
+    color: 'white',
+    marginTop: 30,
+    fontFamily: 'NotoSans-Regular',
+  },
+  source: {
+    fontSize: 14,
+    color: 'white',
+    marginTop: 5,
+    fontFamily: 'NotoSans-Regular',
+  },
+  shareContainer: {
+    position: 'absolute',
+    top: 50,
+    right: 20,
+    height: 40,
+    width: 40,
+    borderRadius: 30,
+    backgroundColor: 'black',
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 50,
+    left: 20,
+    height: 40,
+    width: 40,
+    borderRadius: 30,
+    textAlign: 'center',
+    textAlignVertical: 'center',
+    backgroundColor: 'black',
+  },
+  icon: {
+    height: 25,
+    width: 25,
+  },
+});
 
-export default DetailScreen 
+export default DetailScreen;
